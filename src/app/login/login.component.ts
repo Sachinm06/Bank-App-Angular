e14bc7a5cb127bc005ff4adb9a1232b166418e1e
Sachin M Sibichan <sachinm06@aol.com>
2023-03-03 14:34:34 +0530

    first commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  data="Your Perfect Banking Partner"
+  data1="Enter your account number"
+  acno:any
+  passwd:any
+
+  userDetails={
+    1000:{username:"anu",acno:1000,password:"abc123",balance:0},
+    1001:{username:"arun",acno:1001,password:"abc123",balance:0},
+    1002:{username:"amal",acno:1002,password:"abc123",balance:0},
+    1003:{username:"mega",acno:1003,password:"abc123",balance:0},
+
+  }
+
+login(){
+  alert("its working")
 }
+acnoChange(event:any){
+ this.acno=event.target.value;
+ console.log(this.acno); 
+}
+pass(event:any){
+  this.passwd=event.target.value
+  console.log(this.passwd);
+  
+}
+
+}
+

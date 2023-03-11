cc2ec30e706253cd0d9bea6f8aee3fffe88ec76e
Sachin M Sibichan <sachinm06@aol.com>
2023-03-11 13:04:12 +0530

    new change

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,12 +9,21 @@ import { DataService } from '../Services/data.service';
 export class DashboardComponent {
 
 user:any
+
+acno:any
+psw:any
+amt:any
 constructor(private ds:DataService){
 
   //access data from dataservice and store in a variable
 
 this.user=this.ds.currentUser
 
+}
+deposit(){
+  var acno=this.acno
+  var psw=this.psw
+  var amt=this.amt
 }
 
 }
